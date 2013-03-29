24800a4d24fc6202c13af87517b21e882c0f0f6e
yasu-s <yasuyuki.saito.2012@gmail.com>
2013-03-29 21:20:29 +0900

    add StringExtension

---
+++ Sources/Yazaike.Commons.Test/Extensions/StringExtensionTest.cs
@@ -0,0 +1,55 @@
+﻿namespace Yazaike.Commons.Test.Extensions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+    using System.Text;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Yazaike.Commons.Extensions;
+
+    /// <summary>
+    /// StringExtensionTest の概要の説明
+    /// </summary>
+    [TestClass]
+    public class StringExtensionTest
+    {
+        [TestMethod]
+        public void TestToNarrow()
+        {
+            string value = "ＡＢＣ";
+            Assert.AreEqual("ABC", value.ToNarrow());
+
+            value = "ａｂｃ";
+            Assert.AreEqual("abc", value.ToNarrow());
+
+            value = "１２３４５６７８９０";
+            Assert.AreEqual("1234567890", value.ToNarrow());
+
+            value = "アイウエオ";
+            Assert.AreEqual("ｱｲｳｴｵ", value.ToNarrow());
+
+            value = "アイウエオ";
+            Assert.AreEqual("ｱｲｳｴｵ", value.ToNarrow(CultureInfo.CurrentCulture.LCID));
+        }
+
+        [TestMethod]
+        public void TestToWide()
+        {
+            string value = "ABC";
+            Assert.AreEqual("ＡＢＣ", value.ToWide());
+
+            value = "abc";
+            Assert.AreEqual("ａｂｃ", value.ToWide());
+
+            value = "1234567890";
+            Assert.AreEqual("１２３４５６７８９０", value.ToWide());
+
+            value = "ｱｲｳｴｵ";
+            Assert.AreEqual("アイウエオ", value.ToWide());
+
+            value = "ｱｲｳｴｵ";
+            Assert.AreEqual("アイウエオ", value.ToWide(CultureInfo.CurrentCulture.LCID));
+        }
+    }
+}
---
+++ Sources/Yazaike.Commons/Extensions/StringExtension.cs
@@ -0,0 +1,57 @@
+﻿namespace Yazaike.Commons.Extensions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+    using System.Text;
+    using Microsoft.VisualBasic;
+
+    /// <summary>
+    /// String Extension Class
+    /// </summary>
+    public static class StringExtension
+    {
+        /// <summary>
+        /// Converts wide (full-width) characters in the string to narrow (half-width) characters.
+        /// </summary>
+        /// <param name="value">String expression to be converted.</param>
+        /// <returns>Returns a string converted as specified.</returns>
+        public static string ToNarrow(this string value)
+        {
+            return value.ToNarrow(CultureInfo.CurrentCulture.LCID);
+        }
+
+        /// <summary>
+        /// Converts wide (full-width) characters in the string to narrow (half-width) characters.
+        /// </summary>
+        /// <param name="value">String expression to be converted.</param>
+        /// <param name="localeID">The LocaleID value, if different from the system LocaleID value.</param>
+        /// <returns>Returns a string converted as specified.</returns>
+        public static string ToNarrow(this string value, int localeID)
+        {
+            return Strings.StrConv(value, VbStrConv.Narrow, localeID);
+        }
+
+        /// <summary>
+        /// Converts narrow (half-width) characters in the string to wide (full-width) characters.
+        /// </summary>
+        /// <param name="value">String expression to be converted.</param>
+        /// <returns>Returns a string converted as specified.</returns>
+        public static string ToWide(this string value)
+        {
+            return value.ToWide(CultureInfo.CurrentCulture.LCID);
+        }
+
+        /// <summary>
+        /// Converts wide (full-width) characters in the string to narrow (half-width) characters.
+        /// </summary>
+        /// <param name="value">String expression to be converted.</param>
+        /// <param name="localeID">The LocaleID value, if different from the system LocaleID value.</param>
+        /// <returns>Returns a string converted as specified.</returns>
+        public static string ToWide(this string value, int localeID)
+        {
+            return Strings.StrConv(value, VbStrConv.Wide, localeID);
+        }
+    }
+}
