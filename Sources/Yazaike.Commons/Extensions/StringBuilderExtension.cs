79c63c0142c7cf8b5e995968c32c55e629363481
yasu-s <yasuyuki.saito.2012@gmail.com>
2013-03-31 16:14:32 +0900

    add wpf class library.

---
+++ Sources/Yazaike.Commons/Extensions/StringBuilderExtension.cs
@@ -0,0 +1,54 @@
+﻿namespace Yazaike.Commons.Extensions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    /// <summary>
+    /// StringBuilder Extension Class
+    /// </summary>
+    public static class StringBuilderExtension
+    {
+        /// <summary>
+        /// Appends the string returned by processing a composite format string, 
+        /// which contains zero or more format items, to this instance. 
+        /// Each format item is replaced by the string representation of a single argument.
+        /// And appends the default line terminator to the end of the current StringBuilder object.
+        /// </summary>
+        /// <param name="builder">StringBuilder object.</param>
+        /// <param name="format">A composite format string.</param>
+        /// <param name="arg0">An object to format.</param>
+        /// <returns>
+        /// A reference to this instance with format appended.<br />
+        /// Each format item in format is replaced by the string representation of arg0.
+        /// </returns>
+        public static StringBuilder AppendLine(this StringBuilder builder, string format, object arg0)
+        {
+            if (format == null) throw new ArgumentNullException("format is null.");
+
+            return builder.AppendFormat(format, arg0).AppendLine();
+        }
+
+        /// <summary>
+        /// Appends the string returned by processing a composite format string, 
+        /// which contains zero or more format items, to this instance. 
+        /// Each format item is replaced by the string representation of a corresponding argument in a parameter array.
+        /// And appends the default line terminator to the end of the current StringBuilder object.
+        /// </summary>
+        /// <param name="builder">StringBuilder object.</param>
+        /// <param name="format">A composite format string.</param>
+        /// <param name="args">An array of objects to format.</param>
+        /// <returns>
+        /// A reference to this instance with format appended.<br />
+        /// Each format item in format is replaced by the string representation of the corresponding object argument.
+        /// </returns>
+        public static StringBuilder AppendLine(this StringBuilder builder, string format, params object[] args)
+        {
+            if (format == null) throw new ArgumentNullException("format is null.");
+            if (args == null) throw new ArgumentNullException("args is null.");
+
+            return builder.AppendFormat(format, args).AppendLine();
+        }
+    }
+}
--- Sources/Yazaike.Commons/Extensions/StringExtension.cs
+++ Sources/Yazaike.Commons/Extensions/StringExtension.cs
@@ -53,5 +53,162 @@
         {
             return Strings.StrConv(value, VbStrConv.Wide, localeID);
         }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="arg0"></param>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public static bool Or(this string value, string arg0, params string[] args)
+        {
+            if (value.Equals(arg0)) return true;
+            if (args == null) return false;
+
+            foreach (string str in args)
+            {
+                if (value.Equals(str))
+                    return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="arg0"></param>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public static bool And(this string value, string arg0, params string[] args)
+        {
+            if (!value.Equals(arg0)) return false;
+
+            foreach (string str in args)
+            {
+                if (!value.Equals(str))
+                    return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Converts the string representation of a number to its 16-bit signed integer equivalent.
+        /// </summary>
+        /// <param name="value">
+        /// A string containing a number to convert. 
+        /// </param>
+        /// <returns>
+        /// A 16-bit signed integer equivalent to the number contained in value.
+        /// </returns>
+        public static short ToShort(this string value)
+        {
+            short result = 0;
+
+            if (short.TryParse(value, out result))
+                return result;
+            else
+                throw new FormatException("value is not in the correct format.");
+        }
+
+        /// <summary>
+        /// Converts the string representation of a number to its 32-bit signed integer equivalent.
+        /// </summary>
+        /// <param name="value">
+        /// A string containing a number to convert. 
+        /// </param>
+        /// <returns>
+        /// A 32-bit signed integer equivalent to the number contained in value.
+        /// </returns>
+        public static int ToInt(this string value)
+        {
+            int result = 0;
+
+            if (int.TryParse(value, out result))
+                return result;
+            else
+                throw new FormatException("value is not in the correct format.");
+        }
+
+        /// <summary>
+        /// Converts the string representation of a number to its 64-bit signed integer equivalent.
+        /// </summary>
+        /// <param name="value">
+        /// </param>
+        /// A string containing a number to convert. 
+        /// <returns>
+        /// A 64-bit signed integer equivalent to the number contained in value.
+        /// </returns>
+        public static long ToLong(this string value)
+        {
+            long result = 0;
+
+            if (long.TryParse(value, out result))
+                return result;
+            else
+                throw new FormatException("value is not in the correct format.");
+        }
+
+        /// <summary>
+        /// Converts the string representation of a number to its double-precision floating-point number equivalent.
+        /// </summary>
+        /// <param name="value">
+        /// A string that contains a number to convert. 
+        /// </param>
+        /// <returns>
+        /// A double-precision floating-point number that is equivalent to the numeric value or symbol specified in value.
+        /// </returns>
+        public static double ToDouble(this string value)
+        {
+            double result = 0;
+
+            if (double.TryParse(value, out result))
+                return result;
+            else
+                throw new FormatException("value is not in the correct format.");
+        }
+
+        /// <summary>
+        /// Converts the string representation of a number to its Decimal equivalent.
+        /// </summary>
+        /// <param name="value">
+        /// The string representation of the number to convert.
+        /// </param>
+        /// <returns>
+        /// The equivalent to the number contained in value.
+        /// </returns>
+        public static decimal ToDecimal(this string value)
+        {
+            decimal result = 0;
+
+            if (decimal.TryParse(value, out result))
+                return result;
+            else
+                throw new FormatException("value is not in the correct format.");
+        }
+
+        /// <summary>
+        /// Converts the specified string representation of a logical value to its Boolean equivalent, or throws an exception 
+        /// if the string is not equivalent to the value of Boolean.TrueString or Boolean.FalseString.
+        /// </summary>
+        /// <param name="value">
+        /// A string containing the value to convert. 
+        /// </param>
+        /// <returns>
+        /// true if value is equivalent to the value of the Boolean.TrueString field.<br /> 
+        /// false if value is equivalent to the value of the Boolean.FalseString field.
+        /// </returns>
+        public static bool ToBool(this string value)
+        {
+            bool result = false;
+
+            if (bool.TryParse(value, out result))
+                return result;
+            else
+                throw new FormatException("value is not in the correct format.");
+        }
     }
 }
