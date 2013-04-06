052bee87d5e337847908b34cf4cb72bedbf5a94e
yasu-s <yasuyuki.saito.2012@gmail.com>
2013-04-06 21:33:58 +0900

    Mod DateTimeExtension Class.

--- Sources/Yazaike.Commons/Extensions/DateTimeExtension.cs
+++ Sources/Yazaike.Commons/Extensions/DateTimeExtension.cs
@@ -4,7 +4,6 @@
     using System.Collections.Generic;
     using System.Linq;
     using System.Text;
-    using Microsoft.VisualBasic;
 
     /// <summary>
     /// DateTime Extension Class
@@ -12,13 +11,13 @@
     public static class DateTimeExtension
     {
         /// <summary>
-        /// Gets the first of the month.
+        /// Gets the beginning of the month.
         /// </summary>
         /// <param name="date">DateTime object.</param>
-        /// <returns>the first of the month</returns>
-        public static DateTime GetFirstOfMonth(this DateTime date)
+        /// <returns>the beginning of the month</returns>
+        public static DateTime GetBeginningOfMonth(this DateTime date)
         {
-            return DateAndTime.DateSerial(date.Year, date.Month, 1);
+            return new DateTime(date.Year, date.Month, 1);
         }
 
         /// <summary>
@@ -31,7 +30,47 @@
             if ((DateTime.MaxValue.Year == date.Year) && (DateTime.MaxValue.Month == date.Month))
                 return DateTime.MaxValue.Date;
             else
-                return DateAndTime.DateSerial(date.Year, date.Month + 1, 0);
+                return Microsoft.VisualBasic.DateAndTime.DateSerial(date.Year, date.Month + 1, 0);
+        }
+
+        /// <summary>
+        /// Gets the beginning of the year.
+        /// </summary>
+        /// <param name="date">DateTime object.</param>
+        /// <returns>the beginning of the year</returns>
+        public static DateTime GetBeginningOfYear(this DateTime date)
+        {
+            return new DateTime(date.Year, 1, 1);
+        }
+
+        /// <summary>
+        /// Gets the end of the year.
+        /// </summary>
+        /// <param name="date">DateTime object.</param>
+        /// <returns>the end of the year</returns>
+        public static DateTime GetEndOfYear(this DateTime date)
+        {
+            return new DateTime(date.Year, 12, 31);
+        }
+
+        /// <summary>
+        /// Gets the beginning of the japan fiscal year.
+        /// </summary>
+        /// <param name="date">DateTime object.</param>
+        /// <returns>the beginning of the japan fiscal year</returns>
+        public static DateTime GetBeginningOfJapanFiscalYear(this DateTime date)
+        {
+            return new DateTime(date.Year, 4, 1);
+        }
+
+        /// <summary>
+        /// Gets the end of the japan fiscal year.
+        /// </summary>
+        /// <param name="date">DateTime object.</param>
+        /// <returns>the end of the japan fiscal year</returns>
+        public static DateTime GetEndOfJapanFiscalYear(this DateTime date)
+        {
+            return new DateTime(date.Year, 3, 31);
         }
     }
 }
